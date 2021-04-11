f51973bd35d9312541d39bd2853095bda5bb4dda
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-11 18:07:38 +0900

    eslintの設定

--- App.jsx
+++ App.jsx
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* 下記は、eslintを１行だけ無効化にする記述 */}
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
