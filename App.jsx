4e9dc8e19e1886d399df89246541a47ec5097c35
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-12 15:20:42 +0900

    レイアウトの変更

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MenoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MenoCreateScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDitailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <LogInScreen />
   );
 }
