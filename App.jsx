0776eece6025d4db9456a33e83df1c088a83812c
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-12 14:28:15 +0900

    メモ機能を作成

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDitailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MenoCreateScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDitailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
